45eef4673468e662f78a2b62b0ad85b18849fdcf
Sepid <sepidehsm2007@yahoo.com>
2023-05-21 23:30:57 +0400

    more screenshots

--- analytics/app.py
+++ analytics/app.py
@@ -11,6 +11,10 @@ from config import app, db
 
 port_number = int(os.environ.get("APP_PORT", 5153))
 
+@app.route("/")
+def root():
+    return "This is my Coworking Space project - Sepid"
+
 @app.route("/health_check")
 def health_check():
     return "ok"
